d4a422e46b9f9bcfee6b6aa66bec1f7d247c9464
Md999999 <160726056+Md999999@users.noreply.github.com>
2025-05-13 14:33:24 +0100

    Update main3.py
    send sos

--- main3.py
+++ main3.py
@@ -4,3 +4,18 @@ from typing import List, Dict
 from pydantic import BaseModel
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
+class SOSRequest(BaseModel):
+  message:str
+#user sends a message
+class SOSEvent(BaseModel):
+  message:str
+  timestamp:str
+#what is stored and returned
+sos_logs: Dict[str, List[Dict]] = {}
+#dictionary stores sos logs fpr each user
+@app.post("/sos")
+def send_sos(sos: SOSRequest, username: str = Depends(get_current_user)):
+  event = {
+    "message" : sos.message
+    "timestamp" : datetime.utcnow().isoformat()
+  }
