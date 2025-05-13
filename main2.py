fbc323cb723a95fe7364e938b913d47f854d0a07
Md999999 <160726056+Md999999@users.noreply.github.com>
2025-05-13 12:07:24 +0100

    Update main2.py

--- main2.py
+++ main2.py
@@ -1,1 +1,14 @@
+from fastapi import FastAPI, HTTPException, Depends, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jose import jwt, JWTError
+from datetime import datetime, timedelta
+from pydantic import BaseModel
+from typing import List
+#libraries for jwt seperate from the userauthentication to make it cleaner but its at end of code 
+class EmergencyContact(BaseModel):
+    name: str
+    phone: str
+    relationship: str
+#pydantic creates the class emergency contact to have values like name phone and relation
+#pydantic makes sure the input has to match the structure
 
